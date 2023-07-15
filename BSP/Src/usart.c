34bd60b0a8322086499cb2689ef08d869f697f9d
AlgoOy <2407383500@qq.com>
2023-07-15 11:17:15 +0800

    增加调试功能

---
+++ BSP/Src/usart.c
@@ -0,0 +1,134 @@
+#include "usart.h"
+#include "os.h"
+
+static uint8_t DEBUG_USART_STATE = PERIPHERAL_DEINITIALIZED;
+
+static UART_HandleTypeDef UartHandle = {0};
+
+
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#define GETCHAR_PROTOTYPE int __io_getchar(void)
+
+PUTCHAR_PROTOTYPE {
+  HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
+
+GETCHAR_PROTOTYPE {
+  int ch;
+  while (__HAL_UART_GET_FLAG(&UartHandle, UART_FLAG_RXNE) == RESET);
+  if(HAL_UART_Receive(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF) != HAL_OK) {
+    return -1;
+  }
+  return ch;
+};
+
+
+
+UART_HandleTypeDef *GET_DEBUG_USART_HANDLE(void) {
+  return &UartHandle;
+}
+
+PeripheralCfgStatus DEBUG_USART_CONFIG_INIT(void) {
+  if (DEBUG_USART_STATE == PERIPHERAL_INITIALIZED) {
+    return PERIPHERAL_CFG_ALREADY_INITIALIZED;
+  }
+
+  UartHandle.Instance = DEBUG_USART;
+  UartHandle.Init.BaudRate = DEBUG_USART_BAUDRATE;
+  UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+  UartHandle.Init.StopBits = UART_STOPBITS_1;
+  UartHandle.Init.Parity = UART_PARITY_NONE;
+  UartHandle.Init.Mode = UART_MODE_TX_RX;
+  UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+#if defined(STM32L496xx)
+  UartHandle.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  UartHandle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+#endif
+
+  if (HAL_UART_Init(&UartHandle) != HAL_OK) {
+    return PERIPHERAL_CFG_ERROR;
+  }
+
+  DEBUG_USART_STATE = PERIPHERAL_INITIALIZED;
+
+  return PERIPHERAL_CFG_OK;
+}
+
+PeripheralCfgStatus DEBUG_USART_CONFIG_DEINIT(void) {
+  if (DEBUG_USART_STATE == PERIPHERAL_DEINITIALIZED) {
+    return PERIPHERAL_CFG_NOT_INITIALIZED;
+  }
+
+  if (HAL_UART_DeInit(&UartHandle) != HAL_OK) {
+    return PERIPHERAL_CFG_ERROR;
+  }
+
+  DEBUG_USART_STATE = PERIPHERAL_DEINITIALIZED;
+
+  return PERIPHERAL_CFG_OK;
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart) {
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  if (huart->Instance == DEBUG_USART) {
+    DEBUG_USART_CLK_ENABLE();
+
+    DEBUG_USART_RX_GPIO_CLK_ENABLE();
+    DEBUG_USART_TX_GPIO_CLK_ENABLE();
+
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+    GPIO_InitStruct.Alternate = DEBUG_USART_TX_AF;
+    GPIO_InitStruct.Pin = DEBUG_USART_TX_PIN;
+    HAL_GPIO_Init(DEBUG_USART_TX_GPIO_PORT, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Alternate = DEBUG_USART_RX_AF;
+    GPIO_InitStruct.Pin = DEBUG_USART_RX_PIN;
+    HAL_GPIO_Init(DEBUG_USART_RX_GPIO_PORT, &GPIO_InitStruct);
+
+    HAL_NVIC_SetPriority(DEBUG_USART_IRQn, PreemptPriority, SubPriority);
+    HAL_NVIC_EnableIRQ(DEBUG_USART_IRQn);
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart) {
+  if (huart->Instance == DEBUG_USART) {
+    HAL_NVIC_DisableIRQ(DEBUG_USART_IRQn);
+
+    DEBUG_USART_FORCE_RESET();
+    DEBUG_USART_RELEASE_RESET();
+
+    DEBUG_USART_CLK_DISABLE();
+
+    HAL_GPIO_DeInit(DEBUG_USART_TX_GPIO_PORT, DEBUG_USART_TX_PIN);
+    HAL_GPIO_DeInit(DEBUG_USART_RX_GPIO_PORT, DEBUG_USART_RX_PIN);
+
+    DEBUG_USART_RX_GPIO_CLK_DISABLE();
+    DEBUG_USART_TX_GPIO_CLK_DISABLE();
+  }
+}
+
+void DEBUG_USART_IRQHandler(void) {
+#if OS_CRITICAL_METHOD == 3
+  OS_CPU_SR cpu_sr = 0;
+#endif
+
+  OS_ENTER_CRITICAL();
+  OSIntEnter();
+  OS_EXIT_CRITICAL();
+
+  /* interrupt handler code begin */
+  HAL_UART_IRQHandler(&UartHandle);
+  /* interrupt handler code end */
+
+  OSIntExit();
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+
+}
